7509078af8db486eeb66c1dc43b05f42d432fcef
gwangtan <>
2026-04-28 16:02:05 +0900

    04_28

---
+++ 2025137027/Assets/Script/lerp/LerpMover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LerpMover : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform startPos;
+    public Transform endPos;
+
+    [SerializeField] private float duration = 2f;
+    [SerializeField] private float t = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (t<1f)
+        {
+            t += Time.deltaTime / duration;
+
+            Vector3 a= startPos.position;
+            Vector3 b= endPos.position;
+            Vector3 p = (1f - t) * a + t * b;
+
+            transform.position = p;
+        }
+
+        t += Time.deltaTime / duration;
+        t = Mathf.PingPong(Time.time    / duration, 1f);
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+
+
+    }
+}
---
+++ 2025137027/Assets/Script/lerp/PredictionLineRenderer.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof(LineRenderer))]
+public class PredictionLineRenderer : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+    [Range(1f, 5f)] public float extend = 1.5f;
+
+    private LineRenderer lr;
+
+    public CameraSlrrp cameraSlrrp;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 2;
+        lr.widthMultiplier = 0.05f;
+        lr.material = new Material(Shader.Find("Unlit/Color"))
+        {
+            color = Color.red
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(!startPos || !endPos) return;
+      Vector3 a = startPos.position;
+        Vector3 b = endPos.position;
+        Vector3 pred = Vector3.LerpUnclamped(a,b, extend);
+        lr.SetPosition(0, a);
+        lr.SetPosition(1, pred);
+    }
+
+   public void OnRightClick(InputValue value)
+    {
+        if (!value.isPressed) return;
+
+        Debug.Log("P-1");
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                endPos = hit.transform;
+                startPos = transform;
+                cameraSlrrp.target = endPos;
+                
+                // Å¸°ÙÆÃ
+
+            }
+        }
+        else
+        {
+            return;
+        }
+    } 
+}
